900e714c34c46ca0d91fd566f55735556fdd416f
sgmr29 <sgmr29@b64e8d1a-19a9-11df-a214-3711e6809fde>
2010-02-22 22:56:06 +0000

    Created a version of the code that works on the campus machines.

---
+++ trunk/forPython2.5/server/game/match.py
@@ -0,0 +1,159 @@
+from base import *
+from matchUtils import *
+import serverBoard
+import config.config
+from collections import defaultdict
+from sexpr.sexpr import *
+import os
+import itertools
+import scribe
+
+ServerBoard = serverBoard.ServerBoard
+Scribe = scribe.Scribe
+
+def loadClassDefaults(cfgFile = "config/defaults.cfg"):
+    cfg = config.config.readConfig(cfgFile)
+    for className in cfg.keys():
+        for attr in cfg[className]:
+            setattr(eval(className), attr, cfg[className][attr])
+
+class Match(DefaultGameWorld):
+    def __init__(self, id, controller):
+	DefaultGameWorld.__init__(self)
+        self.id = int(id)
+	self.controller = controller
+	self.scribe = Scribe(self.logPath())
+	self.addPlayer(self.scribe, "spectator")
+
+    def addPlayer(self, connection, type="player"):
+	connection.type = type
+        if (len(self.players) >= 2 and cmp(type, "player") == 0):
+            return "Game is full"
+        if (cmp(type, "player") == 0):
+            self.players.append(connection)
+        elif (cmp(type, "spectator") == 0):
+            self.spectators.append(connection)
+        return True
+
+    def removePlayer(self, connection):
+        if (cmp(connection.type, "player"))==0:
+            self.players.remove(connection)
+        else:
+            self.spectators.remove(connection)
+
+    def start(self):
+        if len(self.players) < 2:
+            return "Game is not full"
+        if (self.winner is not None or self.turn is not None):
+            return "Game has already begun"
+	for i in self.players:
+	    i.score = 0
+        self.mapGeneration()
+        self.turnNum = -1
+
+        self.sendIdent(itertools.chain(self.players, self.spectators))
+        self.nextTurn()
+        return True
+
+    def mapGeneration(self):
+	self.myBoard = ServerBoard(self)
+	self.myBoard.points[24] = -2
+	self.myBoard.points[19] = 5
+	self.myBoard.points[17] = 3
+	self.myBoard.points[13] = -5
+	for i in xrange(13):
+	    self.myBoard.points[i] = -self.myBoard.points[25-i]
+	self.addObject(self.myBoard)
+
+    def nextTurn(self):
+        self.turnNum += 1
+	for obj in self.objects.values():
+            obj.nextTurn()
+
+	if (self.turnNum == 0):
+	    while (self.myBoard.dice[0] == self.myBoard.dice[1]):
+		self.myBoard.rollDice()
+	    if self.myBoard.dice[1] > self.myBoard.dice[0]:
+		self.turnNum += 1
+		self.turn = self.players[0]#Will be changed two lines down
+        if (self.turn == self.players[0]):
+            self.turn = self.players[1]
+        else:
+            self.turn = self.players[0]
+
+        self.sendStatus(itertools.chain(self.players, self.spectators))
+
+        for obj in self.objects.values():
+            obj.changed = False
+        self.animations = ["animations"]
+        self.checkWinner()
+	return True
+
+
+    def checkWinner(self):
+        if (self.players[0].score == 15):
+            self.declareWinner(self.players[0])
+        elif (self.players[1].score == 15):
+            self.declareWinner(self.players[1])
+    
+    def declareWinner(self, winner):
+        self.winner = winner
+	self.turn = None
+
+	msg = ["game-over", self.id, self.winner.user, self.getPlayerIndex(self.winner)]
+	self.scribe.writeSExpr(msg)
+	self.scribe.finalize()
+	self.removePlayer(self.scribe)
+
+        for p in self.players:
+            p.writeSExpr(msg)
+        
+    def logPath(self):
+        return "logs/" + str(self.id) + ".gamelog"
+    
+    def move(self, fromPoint, toPoint):
+        return self.myBoard.move(fromPoint, toPoint)
+
+    def bearOff(self, fromPoint):
+	return self.myBoard.bearOff(fromPoint)
+
+    def sendIdent(self, players):
+        if len(self.players) < 2:
+            return False
+        list = []
+        for i in itertools.chain(self.players, self.spectators):
+            list += [[self.getPlayerIndex(i), i.user, i.screenName, i.type]]
+        for i in players:
+            i.writeSExpr(['ident', list, self.id, self.getPlayerIndex(i)])
+
+    def getPlayerIndex(self, player):
+        try:
+            playerIndex = self.players.index(player)
+        except ValueError:
+            playerIndex = -1
+	return playerIndex
+
+    def sendStatus(self, players):
+        for i in players:
+            i.writeSExpr(self.status())
+            i.writeSExpr(self.animations)
+
+    def status(self):
+        msg = ["status"]
+
+        msg.append(["game", self.turnNum, self.players[0].score, 
+		    self.players[1].score])
+        typeLists = defaultdict(list)
+        for obj in self.objects.values():
+            typeLists[obj.__class__].append(obj)
+        for type in typeLists.keys():
+            if (len(typeLists[type]) > 0):
+                msg.append([type.__name__] +
+                           [j.toList() for j in typeLists[type]])
+        return msg
+
+    def talk(self, message):
+        return self.myBoard.talk(message)
+
+loadClassDefaults()
+
---
+++ trunk/forPython2.5/server/game/serverBoard.py
@@ -0,0 +1,117 @@
+import functools
+import gameObject
+import math
+import random
+GameObject = gameObject.GameObject
+
+
+class ServerBoard(GameObject):
+    """
+    The backgammon board
+    """
+
+    def __init__(self, game):
+        GameObject.__init__(self, game)
+	self.points = [0]*26
+	self.dice = [0]*4
+
+    def toList(self):
+        list = GameObject.toList(self)
+	list.extend(self.dice)
+        list.extend(self.points)
+        return list
+
+    def talk(self, message):
+	self.game.animations.append(['talk', self.id, message])
+	return True
+
+    def nextTurn(self):
+        GameObject.nextTurn(self)
+	#Force the use of all unused dice
+	movingPlayer = self.game.getPlayerIndex(self.game.turn)
+        direction = 2*movingPlayer - 1
+	i = 0
+	while (i < 4 and self.dice[i] > 0):
+	    success = False
+	    start = 25-25*movingPlayer
+	    stop = 26*movingPlayer - 1
+	    for fromPoint in xrange(start, stop, direction):
+		toPoint = fromPoint + direction*self.dice[i]
+		success = success or self.move(fromPoint,toPoint)==True
+		success = success or self.bearOff(fromPoint)==True
+		if success:
+		    break
+	    if not success:
+		i += 1
+
+	self.rollDice()
+
+    def rollDice(self):
+	self.dice = [random.randint(1,6), random.randint(1,6)]
+	if self.dice[0] == self.dice[1]:
+	    self.dice = self.dice*2
+	else:
+	    self.dice.extend([0]*2)
+
+    def move(self, fromPoint, toPoint):
+	movingPlayer = self.game.getPlayerIndex(self.game.turn)
+	direction = 2*movingPlayer - 1
+	if (toPoint-fromPoint) * direction < 0:
+	    return "Tried to move the wrong direction"
+	if fromPoint == toPoint:
+	    return "Tried to move nowhere"
+	if fromPoint < 0 or fromPoint > 25 or toPoint < 1 or toPoint > 24:
+	    return "Referred to a point that does not exist."
+	if int(abs(fromPoint-toPoint)) not in self.dice:
+	    return "Move does not match any die"
+	if self.points[fromPoint] * direction < 1:
+	    return "You have no checkers to move from there"
+	if self.points[toPoint] * direction < -1:
+	    return "Your opponent has two or more checkers at destination."
+	if abs(self.points[25-25*movingPlayer]) > 0 and \
+	    fromPoint != 25-25*movingPlayer:
+	    return "You have to clear your bar before making other moves"
+	#Allow the move
+	if self.points[toPoint] * direction == -1:
+	    #The opponent is hit
+	    self.points[25*movingPlayer] += self.points[toPoint]
+	    self.points[toPoint] = 0
+	self.points[toPoint] += direction
+	self.points[fromPoint] -= direction
+	self.dice.remove(int(abs(fromPoint-toPoint)))
+	self.dice.append(0)
+	return True
+
+    def bearOff(self, fromPoint):
+	movingPlayer = self.game.getPlayerIndex(self.game.turn)
+        direction = 2*movingPlayer - 1
+	lastChecker = 0#distance of current player's furthest checker from home
+	for i in xrange(25):
+	    distance = i + movingPlayer * (25 - 2 * i)
+	    if direction * self.points[i] > 0 and distance > lastChecker:
+		lastChecker = distance
+
+        if fromPoint < 1 or fromPoint > 24:
+            return "Referred to a point that does not exist."
+	if lastChecker > 6:
+	    return "Everything must be on your home board before bearing off"
+	dieToUse = -1
+	distance = fromPoint + movingPlayer * (25- 2*fromPoint)
+	if distance in self.dice:
+	    dieToUse = distance
+	if distance == lastChecker and max(self.dice) > distance:
+	    dieToUse = int(max(self.dice))
+	if dieToUse == -1:
+            return "The dice prevent this bear off"
+        if self.points[fromPoint] * direction < 1:
+            return "You have no checkers to move from there"
+        if abs(self.points[25-25*movingPlayer]) > 0 and \
+            fromPoint != 25-25*movingPlayer:
+            return "You have to clear your bar before making other moves"
+        #Allow the move
+        self.game.turn.score += 1
+        self.points[fromPoint] -= direction
+        self.dice.remove(dieToUse)
+        self.dice.append(0)
+        return True
+
